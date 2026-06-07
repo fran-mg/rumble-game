ef7e65b00d9f0419220325aaff567df815dc1da6
fran-mg <30898104+fran-mg@users.noreply.github.com>
2026-06-07 11:18:30 +0100

    Add app icons

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import HomeHeader from "./modes/_ModeHeader";
 import ModeCardList from "./modes/_ModeCardList";
 
 // true = user view, false = show dev tools
-const isProductionTest = false;
+const isProductionTest = true;
 
 export default function HomeScreen() {
   return (
